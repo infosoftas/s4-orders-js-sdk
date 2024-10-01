b61ef398eb913312c71775ca2a28b6209649fbb3
mikayel_zarafyan <mikayelzarafyan@gmail.com>
2024-10-01 11:23:28 +0400

    change fields import

--- orders-module/src/components/FormFields/FormFieldsMapper.ts
+++ orders-module/src/components/FormFields/FormFieldsMapper.ts
@@ -1,9 +1,8 @@
-import { lazy } from 'react';
 import { FIELD_TYPES } from './FormFields.helper';
 
-const EmailField = lazy(() => import('./EmailField'));
-const PhoneField = lazy(() => import('./PhoneField'));
-const NameField = lazy(() => import('./NameField'));
+import EmailField from 'Component/FormFields/EmailField';
+import PhoneField from 'Component/FormFields/PhoneField';
+import NameField from 'Component/FormFields/NameField';
 
 const formFieldsMapper = {
     [FIELD_TYPES.PHONE]: PhoneField,
