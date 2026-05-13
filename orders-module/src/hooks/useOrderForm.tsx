110425e36d11e1aace9ceaa4d4d409f1c6fb4af8
Benedicte Vikvam <benedictevikvam@gmail.com>
2026-05-13 12:58:44 +0200

    Set the package id to fallback offer as soon as its fetched

--- orders-module/src/hooks/useOrderForm.tsx
+++ orders-module/src/hooks/useOrderForm.tsx
@@ -327,6 +327,9 @@ const useOrderForm = ({
                     void fetchDenialFallbackOffer(organizationId)
                         .then((offer) => {
                             setOrderDenialFallbackOffer(offer);
+                            if (offer?.templatePackageId) {
+                                fallbackOfferTemplatePackageIdRef.current = offer.templatePackageId;
+                            }
                         })
                         .catch(() => {
                             setOrderDenialFallbackOffer(undefined);
